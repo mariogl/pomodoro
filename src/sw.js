3eb2d89ebf21764bfa0b302eec26f9af96cfd2ab
mariogl <mariogl84@hotmail.com>
2019-10-22 22:29:26 +0200

    Web manifest e iconos

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+workbox.core.skipWaiting();
+
 workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
